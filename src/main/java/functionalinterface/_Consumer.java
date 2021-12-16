daad036bfa251a149c3ccfdfbbedc6b2b0c7228e
Halil Erkan Dursunoğlu <halilerkandev@gmail.com>
2021-12-17 02:06:24 +0300

    added an example for basic usage of BiConsumer

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -7,14 +8,29 @@ public class _Consumer {
         Customer customerJohn = new Customer(
                 "John",
                 "123456789");
+
+        // Normal Java function
         greetCustomer(customerJohn);
+        greetCustomerV2(customerJohn, false);
+
+        // Customer Functional Interface
         greetCustomerConsumer.accept(customerJohn);
+        greetCustomerConsumerV2.accept(customerJohn, false);
     }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.getName() +
+                    ", thanks for registering phone number " +
+                    ( showPhoneNumber ? customer.getPhoneNumber() : "*********" ));
 
     static Consumer<Customer> greetCustomerConsumer = customer ->
             System.out.println("Hello " + customer.getName() +
                     ", thanks for registering phone number " +
                     customer.getPhoneNumber());
+
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+        System.out.println("Hello " + customer.getName() + ", thanks for registering phone number " + ( showPhoneNumber ? customer.getPhoneNumber() : "*********" ));
+    }
 
     static void greetCustomer(Customer customer) {
         System.out.println("Hello " + customer.getName() + ", thanks for registering phone number " + customer.getPhoneNumber());
