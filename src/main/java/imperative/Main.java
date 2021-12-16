e234f7664ebd535e2c8f1f6efe6f1a6fb6d3f831
Halil Erkan Dursunoğlu <halilerkandev@gmail.com>
2021-12-16 19:42:09 +0300

    imperative approach

---
+++ src/main/java/imperative/Main.java
@@ -0,0 +1,53 @@
+package imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static imperative.Main.Gender.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
+        );
+
+        // Imperative Approach
+        List<Person> females = new ArrayList<>();
+
+        for(Person person : people) {
+            if(FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for(Person female : females) {
+            System.out.println(female);
+        }
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
