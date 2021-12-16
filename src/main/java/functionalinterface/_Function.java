f6610aa9180edd7e181bcb7e871bab202eea63f0
Halil Erkan Dursunoğlu <halilerkandev@gmail.com>
2021-12-17 01:38:13 +0300

    added an example for usage of andThen Function method

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -9,9 +9,18 @@ public class _Function {
 
         int increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+
+        System.out.println(addBy1AndThenMultiplyBy10.apply(4));
     }
 
-    static Function<Integer, Integer> incrementByOneFunction = (x) -> x++;
+    static Function<Integer, Integer> incrementByOneFunction = (number) -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function = (number) -> number * 10;
 
     static int incrementByOne(int number) {
         return number + 1;
