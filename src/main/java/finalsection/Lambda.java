0ad8428b15f31b9547bf53a88ef3bfa982285cb9
Halil Erkan Dursunoğlu <halilerkandev@gmail.com>
2021-12-17 20:56:52 +0300

    added sample of differences primitive int and class Integer

--- src/main/java/finalsection/Lambda.java
+++ src/main/java/finalsection/Lambda.java
@@ -1,21 +1,37 @@
 package finalsection;
 
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Lambda {
     public static void main(String[] args) {
 
-        BiFunction<String, Integer, String> upperCaseName = (name, age) -> {
-            // logic
-            if (name.isBlank()) throw new IllegalArgumentException("Name cannot be blank");
-            System.out.println(age);
-            return name.toUpperCase();
-        };
+        Integer x = null;
+        // int y = null; // Compile error
+        int y = 0;
+
+//        BiFunction<String, Integer, String> upperCaseName = (name, age) -> {
+//            // logic
+//            if (name.isBlank()) throw new IllegalArgumentException("Name cannot be blank");
+//            System.out.println(age);
+//            return name.toUpperCase();
+//        };
 
         String upperCasedName = upperCaseName.apply("Alex", 20);
 
         System.out.println(upperCasedName);
 
     }
+
+    static BiFunction<String, Integer, String> upperCaseName = (name, age) -> {
+        // logic
+        if (name.isBlank()) throw new IllegalArgumentException("Name cannot be blank");
+        System.out.println(age);
+        return name.toUpperCase();
+    };
+
+//    class Service {
+//        public Consumer<String> blah = s -> {};
+//    }
 }
